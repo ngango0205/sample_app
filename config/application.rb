8349499151d1e9ff93f7f2db48584669d9b1888d
ngango0205 <quynhnga27231096@gmail.com>
2018-07-09 10:36:36 +0700

    chap 3

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,17 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+# you"ve limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Nga
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.i18n.load_path += Dir[Rails.root.join("my", "locales", "*.{rb,yml}").to_s]
+    config.i18n.available_locales = [:en, :vi, :jp]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
