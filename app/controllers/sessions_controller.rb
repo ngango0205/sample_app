174c00e83c6e2e61964092f041b4de924ae7dcc9
ngango0205 <quynhnga27231096@gmail.com>
2018-07-11 10:13:27 +0700

    chap7

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash[:danger] = t("title1")
+      render "new"
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < ApplicationRecord
   private
 
   def downcase_email
-    self.email = email.downcase!
+    email.downcase!
   end
 end
