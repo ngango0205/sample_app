55fb510fbe97275445b3781120e48180b1e2658d
ngango0205 <quynhnga27231096@gmail.com>
2018-07-12 10:08:42 +0700

    chap8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
   before_action :set_locale
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
-    include SessionsHelper
   end
 end
